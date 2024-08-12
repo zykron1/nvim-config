895457fa219ccb3468085aefc2e330e7b27a6d53
Ahsan Ahmed <ahsan.ahmed3246@gmail.com>
2024-08-12 12:29:54 -0700

    AAAAGGHHGHGHHGHG IT AUTO CHANGED TABS TO SPACES ): WHY IS THIS A DEFAULT SETTING WTH NVIM

--- init.lua
+++ init.lua
@@ -231,12 +231,18 @@ vim.opt.shiftwidth = 4     -- Number of spaces to use for each step of (auto)ind
 vim.opt.softtabstop = 4    -- Insert/delete 4 spaces for a tab press (when in insert mode)
 vim.opt.expandtab = false  -- Don't convert tabs to spaces
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "html",
+    pattern = "*",
     callback = function()
         vim.opt.tabstop = 4
         vim.opt.shiftwidth = 4
         vim.opt.softtabstop = 4
         vim.opt.expandtab = false
+    end
+})
+vim.api.nvim_create_autocmd("BufReadPost", {
+    pattern = "*",
+    callback = function()
+        vim.cmd("retab")
     end
 })
 
