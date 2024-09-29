7689ecd7f15bcedd40325b90d4a333b741bb79aa
Ahsan Ahmed <ahsan.ahmed3246@gmail.com>
2024-09-29 09:23:27 -0700

    Fixed issue with module loading

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
+-- Add the 'core' folder to the Lua module path
+local config_path = vim.fn.stdpath("config") .. "/"
+package.path = config_path .. "?.lua;" .. package.path
+
+-- Require the core modules
 require("core.plugins")
-require("core.setups")
-require("core.lsp")
 require("core.keymaps")
+require("core.lsp")
+require("core.setups")
 require("core.placement")
