d71957ecad1326bd8cf96c8a0e37a44fc67cbc19
Ahsan Ahmed <ahsan.ahmed3246@gmail.com>
2024-10-13 12:24:25 -0700

    made nvim-tree accessable again

--- core/placement.lua
+++ core/placement.lua
@@ -16,14 +16,21 @@ vim.api.nvim_create_autocmd("FileType", {
 	end
 })
 
-vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*",
-    callback = function()
-        local bufname = vim.api.nvim_buf_get_name(0)
-        if not bufname:match("NvimTree_") then return end
+local autocmd_id
 
-        -- Move to the next window (typically the main buffer)
-        vim.cmd("wincmd l")
+autocmd_id = vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        -- Move cursor to the window on the right (if available)
+        vim.defer_fn(function()
+            if #vim.api.nvim_list_wins() > 1 then
+                vim.cmd("wincmd l")  -- Move to the right window
+            end
+
+            -- Disable this autocmd after it runs once
+            if autocmd_id then
+                vim.api.nvim_del_autocmd(autocmd_id)
+            end
+        end, 10)  -- Small delay to ensure windows are initialized
     end,
 })
 
